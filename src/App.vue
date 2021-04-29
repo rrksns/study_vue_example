15e0949b97b87ac5ebf4ad9227d7dd2c2032fe1f
seol <slee@dkitec.com>
2021-04-30 06:15:40 +0900

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+<div class="menu" >
+  <a v-for="(item,i) in menus" :key="i">{{item}}</a>
+  
+</div>
+
+  <div>
+    <img src="./assets/room0.jpg" alt="" class="room-img">
+    <h4>{{products[0]}}</h4>
+    <p>70만원</p>
+    <button @click="addCnt">허위매물신고</button><span>신고수 : {{reportCnt[0]}}</span>    
+  </div>
+
+  <div>
+    <img src="./assets/room1.jpg" alt="" class="room-img">
+    <h4>{{products[1]}}</h4>
+    <p>10만원</p>    
+    <button @click="addCnt1">허위매물신고</button><span>신고수 : {{reportCnt[1]}}</span>
+  </div>
+
+  <div>
+    <img src="./assets/room2.jpg" alt="" class="room-img">
+    <h4>{{products[2]}}</h4>
+    <p>xx만원</p>    
+    <button @click="addCnt2">허위매물신고</button><span>신고수 : {{reportCnt[2]}}</span>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      price1 : 60,
+      price2 : 70,
+      reportCnt : [0,0,0],
+      products : ['역삼동 원름','천호동 원룸','상계동 원룸'],
+      menus: ['Home', 'Shop', 'About']
+    }
+  },
+  components: {   
+  },
+  methods: {
+    addCnt(){
+      this.reportCnt[0]++
+    },
+    addCnt1(){
+      this.reportCnt[1]++
+    },
+    addCnt2(){
+      this.reportCnt[2]++
+    }
+  },
 }
 </script>
 
@@ -21,6 +65,24 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
+
+}
+
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+
+}
+
+.menu a{
+  color: white;
+  padding: 10px;
+}
+
+.room-img{
+  width:100%;
+  margin-top: 40px;
 }
 </style>
